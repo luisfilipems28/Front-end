431ed7dabd102877a219692bc3279a95c5a2cbd4
Luis Filipe M. dos Santos <163148824+luisfilipems28@users.noreply.github.com>
2025-04-27 19:10:04 -0300

    Novos exercícios

--- aula09/for.js
+++ aula09/for.js
@@ -1,18 +1,30 @@
-let i;
+const prompt = require('readline-sync')
 
 /*//* expressao 1 : inicialização da variável de controle
 //* expressao 2 : condição de permanência do for
 //*  expressao 3 : o incremento da variavel de controle
 
-for(expressao 1; expressao 2; expressao 3){
+/*for(expressao 1; expressao 2; expressao 3){
     //O escopo de for
-} */
+} 
 
 for(i = 0; i < 5; i ++){
     console.log(i)
-}
+}*/
 
 // Limpando o console
 console.clear()
 
 // * Exemplo
+let i;
+let maiorNumero = 0;
+let numeroInformado;
+
+for(i = 1; i<=5; i++){
+    numeroInformado = Number(prompt.question("Informe um número positivo: "))
+    if(numeroInformado > maiorNumero){
+        maiorNumero = numeroInformado
+    }
+}
+
+console.log("Maior número: "+ numeroInformado)
